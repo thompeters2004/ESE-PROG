40e0a30208eeb32f55f883cc20992900dd4d45f3
Your Name <you@example.com>
2023-10-05 11:09:06 +0200

    Added C++ code to BME280 and HC-SR04.

--- BME280 Raspberry PI C++/src/main.cpp
+++ BME280 Raspberry PI C++/src/main.cpp
@@ -1,8 +1,29 @@
 #include <bme280.h>
 
-int main() {
-    BME280 sensor;
-    sensor.initialize();
-    sensor.readData();
+int main()
+{
+    try
+    {
+        BME280 sensor;
+        sensor.setup();
+
+        float t = sensor.getTemperature();
+        float p = sensor.getPressure();
+        float h = sensor.getHumidity();
+        float a = sensor.getAltitude(p);
+
+        cout << "Sensor: bme280" << endl
+             << "Humidity: " << h << endl
+             << "Pressure: " << p << endl
+             << "Temperature: " << t << endl
+             << "Altitude: " << a << endl
+             << "Timestamp: " << static_cast<int>(time(NULL)) << endl;
+    }
+    catch (const runtime_error &e)
+    {
+        cerr << e.what() << endl;
+        return -1;
+    }
+
     return 0;
 }
--- Test/README.md
+++
@@ -1,2 +0,0 @@
-# PROG5-6-LAB-V
-This is the prog5/6 repository for my assignments.
