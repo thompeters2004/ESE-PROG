bb4744a696c20829da58eb7af497f821ff50c099
Your Name <you@example.com>
2023-10-05 18:14:52 +0200

    Update

--- BME280 Raspberry PI C++/src/bme280.h
+++ BME280 Raspberry PI C++/src/bme280.h
@@ -1,5 +1,5 @@
-#ifndef BME280_H
-#define BME280_H
+#ifndef __BME280_H__
+#define __BME280_H__
 
 #include <stdio.h>
 #include <errno.h>
@@ -9,102 +9,99 @@
 #include <wiringPiI2C.h>
 #include <iostream>
 
-#define BME280_ADDRESS 0x76
+#define BME280_ADDRESS                0x76
 
-#define BME280_REGISTER_DIG_T1 0x88
-#define BME280_REGISTER_DIG_T2 0x8A
-#define BME280_REGISTER_DIG_T3 0x8C
-#define BME280_REGISTER_DIG_P1 0x8E
-#define BME280_REGISTER_DIG_P2 0x90
-#define BME280_REGISTER_DIG_P3 0x92
-#define BME280_REGISTER_DIG_P4 0x94
-#define BME280_REGISTER_DIG_P5 0x96
-#define BME280_REGISTER_DIG_P6 0x98
-#define BME280_REGISTER_DIG_P7 0x9A
-#define BME280_REGISTER_DIG_P8 0x9C
-#define BME280_REGISTER_DIG_P9 0x9E
-#define BME280_REGISTER_DIG_H1 0xA1
-#define BME280_REGISTER_DIG_H2 0xE1
-#define BME280_REGISTER_DIG_H3 0xE3
-#define BME280_REGISTER_DIG_H4 0xE4
-#define BME280_REGISTER_DIG_H5 0xE5
-#define BME280_REGISTER_DIG_H6 0xE7
-#define BME280_REGISTER_CHIPID 0xD0
-#define BME280_REGISTER_VERSION 0xD1
-#define BME280_REGISTER_SOFTRESET 0xE0
-#define BME280_RESET 0xB6
-#define BME280_REGISTER_CAL26 0xE1
-#define BME280_REGISTER_CONTROLHUMID 0xF2
-#define BME280_REGISTER_CONTROL 0xF4
-#define BME280_REGISTER_CONFIG 0xF5
-#define BME280_REGISTER_PRESSUREDATA 0xF7
-#define BME280_REGISTER_TEMPDATA 0xFA
-#define BME280_REGISTER_HUMIDDATA 0xFD
+#define BME280_REGISTER_DIG_T1        0x88
+#define BME280_REGISTER_DIG_T2        0x8A
+#define BME280_REGISTER_DIG_T3        0x8C
+#define BME280_REGISTER_DIG_P1        0x8E
+#define BME280_REGISTER_DIG_P2        0x90
+#define BME280_REGISTER_DIG_P3        0x92
+#define BME280_REGISTER_DIG_P4        0x94
+#define BME280_REGISTER_DIG_P5        0x96
+#define BME280_REGISTER_DIG_P6        0x98
+#define BME280_REGISTER_DIG_P7        0x9A
+#define BME280_REGISTER_DIG_P8        0x9C
+#define BME280_REGISTER_DIG_P9        0x9E
+#define BME280_REGISTER_DIG_H1        0xA1
+#define BME280_REGISTER_DIG_H2        0xE1
+#define BME280_REGISTER_DIG_H3        0xE3
+#define BME280_REGISTER_DIG_H4        0xE4
+#define BME280_REGISTER_DIG_H5        0xE5
+#define BME280_REGISTER_DIG_H6        0xE7
+#define BME280_REGISTER_CHIPID        0xD0
+#define BME280_REGISTER_VERSION       0xD1
+#define BME280_REGISTER_SOFTRESET     0xE0
+#define BME280_RESET                  0xB6
+#define BME280_REGISTER_CAL26         0xE1
+#define BME280_REGISTER_CONTROLHUMID  0xF2
+#define BME280_REGISTER_CONTROL       0xF4
+#define BME280_REGISTER_CONFIG        0xF5
+#define BME280_REGISTER_PRESSUREDATA  0xF7
+#define BME280_REGISTER_TEMPDATA      0xFA
+#define BME280_REGISTER_HUMIDDATA     0xFD
 
-#define MEAN_SEA_LEVEL_PRESSURE 1013
+#define MEAN_SEA_LEVEL_PRESSURE       1013
 
-// Calibration data structure
-struct bme280_calib_data {
-    uint16_t dig_T1;
-    int16_t dig_T2;
-    int16_t dig_T3;
-
-    uint16_t dig_P1;
-    int16_t dig_P2;
-    int16_t dig_P3;
-    int16_t dig_P4;
-    int16_t dig_P5;
-    int16_t dig_P6;
-    int16_t dig_P7;
-    int16_t dig_P8;
-    int16_t dig_P9;
-
-    uint8_t dig_H1;
-    int16_t dig_H2;
-    uint8_t dig_H3;
-    int16_t dig_H4;
-    int16_t dig_H5;
-    int8_t dig_H6;
-};
-
-// Raw data structure
-struct bme280_raw_data {
-    uint8_t pmsb;
-    uint8_t plsb;
-    uint8_t pxsb;
-
-    uint8_t tmsb;
-    uint8_t tlsb;
-    uint8_t txsb;
-
-    uint8_t hmsb;
-    uint8_t hlsb;
-
-    int32_t temperature;
-    int32_t pressure;
-    int32_t humidity;
-};
-
-// BME280 class declaration
 class BME280 {
+public:
+    BME280();
+    bool initialize();
+    void readCalibrationData();
+    float getTemperature();
+    float getPressure();
+    float getHumidity();
+    float getAltitude();
+
 private:
     int fd;
     bme280_calib_data cal;
 
-    void readCalibrationData();
     int32_t getTemperatureCalibration(int32_t adc_T);
     float compensateTemperature(int32_t t_fine);
     float compensatePressure(int32_t adc_P, int32_t t_fine);
     float compensateHumidity(int32_t adc_H, int32_t t_fine);
-    void getRawData(bme280_raw_data &raw);
+    void getRawData(bme280_raw_data *raw);
 
-public:
-    BME280();
-    void setup();
-    float getTemperature();
-    float getPressure();
-    float getHumidity();
-    float getAltitude(float pressure);
+    struct bme280_calib_data {
+  uint16_t dig_T1;
+  int16_t  dig_T2;
+  int16_t  dig_T3;
+
+  uint16_t dig_P1;
+  int16_t  dig_P2;
+  int16_t  dig_P3;
+  int16_t  dig_P4;
+  int16_t  dig_P5;
+  int16_t  dig_P6;
+  int16_t  dig_P7;
+  int16_t  dig_P8;
+  int16_t  dig_P9;
+
+  uint8_t  dig_H1;
+  int16_t  dig_H2;
+  uint8_t  dig_H3;
+  int16_t  dig_H4;
+  int16_t  dig_H5;
+  int8_t   dig_H6;
+    };
+
+    struct bme280_raw_data {
+  uint8_t pmsb;
+  uint8_t plsb;
+  uint8_t pxsb;
+
+  uint8_t tmsb;
+  uint8_t tlsb;
+  uint8_t txsb;
+
+  uint8_t hmsb;
+  uint8_t hlsb;
+
+  uint32_t temperature;
+  uint32_t pressure;
+  uint32_t humidity; 
+    };
 };
 
-#endif // BME280_H
+#endif
--- BME280 Raspberry PI C++/src/main.cpp
+++ BME280 Raspberry PI C++/src/main.cpp
@@ -2,30 +2,25 @@
 
 using namespace std;
 
-int main()
-{
-    try
-    {
-        BME280 sensor;
-        sensor.setup();
+int main() {
+    BME280 sensor;
 
-        float t = sensor.getTemperature();
-        float p = sensor.getPressure();
-        float h = sensor.getHumidity();
-        float a = sensor.getAltitude(p);
-
-        std::cout << "Sensor: bme280" << endl
-             << "Humidity: " << h << endl
-             << "Pressure: " << p << endl
-             << "Temperature: " << t << endl
-             << "Altitude: " << a << endl
-             << "Timestamp: " << static_cast<int>(time(NULL)) << endl;
-    }
-    catch (const runtime_error &e)
-    {
-        cerr << e.what() << endl;
+    if(!sensor.initialize()) {
+        cout << "Device not found" << endl;
         return -1;
     }
+
+    float t = sensor.getTemperature(); // C
+    float p = sensor.getPressure();    // hPa
+    float h = sensor.getHumidity();    // %
+    float a = sensor.getAltitude();    // meters
+
+    cout << "Sensor: bme280" << endl
+         << "Humidity: " << h << endl
+         << "Pressure: " << p << endl
+         << "Temperature: " << t << endl 
+         << "Altitude: " << a << endl 
+         << "Timestamp: " << static_cast<int>(time(NULL)) << endl;
 
     return 0;
 }
